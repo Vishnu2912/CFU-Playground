4e97a9503e5664f786bd99698f275e3ca3c379f0
Vishnu Varma V <85408861+Vishnu2912@users.noreply.github.com>
2022-07-06 18:00:51 +0530

    Update cfu.v

--- proj/simd_filt/cfu.v
+++ proj/simd_filt/cfu.v
@@ -11,7 +11,9 @@ module Cfu (
   input               clk
 );
   localparam InputOffset = $signed(9'd128);
+  //registers to store the distinct filter values for input depth of 12
   reg [31:0] filt_vals[0:323];
+  //registers to store the filter values for input depth of 1
   reg [31:0] filt_vals_1[0:107];
   // SIMD multiply step:
   wire signed [15:0] prod_0, prod_1, prod_2, prod_3;
@@ -27,7 +29,9 @@ module Cfu (
 
   wire signed [31:0] sum_prods;
   wire signed [31:0] sum_prods_1;
+  //Accumulating all the products
   assign sum_prods = prod_0 + prod_1 + prod_2 + prod_3;
+  //MAC operation for input depth of 1
   assign sum_prods_1 = ($signed(cmd_payload_inputs_0) + InputOffset)
                   * $signed(filt_vals_1[cmd_payload_inputs_1]);
 
@@ -43,20 +47,22 @@ module Cfu (
       rsp_valid <= ~rsp_ready;
     end else if (cmd_valid) begin
       rsp_valid <= 1'b1;
-      // Accumulate step:
       if(cmd_payload_function_id[9:3] == 1) begin
-       rsp_payload_outputs_0 <= 32'b0;
+       rsp_payload_outputs_0 <= 32'b0;        //resetting the accumulator
       end
+      //filter storage for input depth of 12
       else if(cmd_payload_function_id[9:3] == 2) begin
-      	//filter value store here
       	filt_vals[cmd_payload_inputs_0] <= cmd_payload_inputs_1;
       end
+      //filter storage for input depth of 1
       else if(cmd_payload_function_id[9:3] == 3) begin
       	filt_vals_1[cmd_payload_inputs_0] <= cmd_payload_inputs_1;
       end
+      //Accumulate step for input depth of 1
       else if(cmd_payload_function_id[9:3] == 4) begin
       	rsp_payload_outputs_0 <= rsp_payload_outputs_0 + sum_prods_1;
       end
+      //Accumulate step for input depth of 12
       else begin
       	rsp_payload_outputs_0 <= rsp_payload_outputs_0 + sum_prods;
       end
